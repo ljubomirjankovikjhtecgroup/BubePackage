7a547eb2fdb5fc1f7f93666cebc90856d28efbdc
Ljubomir Jankovikj <ljubomir.jankovikj@htecgroup.com>
2023-07-13 12:29:12 +0200

    Added a public init for BubesPrinter

--- Sources/BubesPrinter/BubesPrinter.swift
+++ Sources/BubesPrinter/BubesPrinter.swift
@@ -1,6 +1,9 @@
 import Foundation
 
 public struct BubesPrinter {
+    
+    public init() {}
+    
     func printWithSmiley(text: String) -> String {
         return text.replacingOccurrences(of: " ", with: "\u{1F600}")
     }
