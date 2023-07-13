4b0d6f07ac6b281bc788fc9fe392ee4eac77fba4
Ljubomir Jankovikj <ljubomir.jankovikj@htecgroup.com>
2023-07-13 12:43:04 +0200

    Added comments to think about hiding the implemenation and adding a limitation with some keys

--- Sources/BubesPrinter/BubesPrinter.swift
+++ Sources/BubesPrinter/BubesPrinter.swift
@@ -4,6 +4,10 @@ public struct BubesPrinter {
     
     public init() {}
     
+    // OK, this is publicly available!
+    // How can we adapt/change this implementation to hide it?
+    // Maybe add some keys to limit usage to someone?
+    
     public func printWithSmiley(text: String) -> String {
         return text.replacingOccurrences(of: " ", with: "\u{1F600}")
     }
