42746394fcbf2b2f9ccf0726e200d953def2dc43
Ljubomir Jankovikj <ljubomir.jankovikj@htecgroup.com>
2023-07-13 13:45:42 +0200

    Added the BubesFramework - hidden the implementation, reworked the package file and removed the old code with the BubesPrinter folder

--- Package.swift
+++ Package.swift
@@ -5,24 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "BubesPrinter",
+    platforms: [
+          .macOS(.v13), .iOS(.v16)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BubesPrinter",
-            targets: ["BubesPrinter"]),
+            name: "BubesFramework",
+            targets: ["BubesFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "BubesPrinter",
-            dependencies: []),
-        .testTarget(
-            name: "BubesPrinterTests",
-            dependencies: ["BubesPrinter"]),
-    ]
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages this package depends on.
+            .binaryTarget(
+                name: "BubesFramework",
+                path: "./Sources/BubesFramework.xcframework")
+        ]
 )
---
+++ Sources/BubesFramework.xcframework/ios-arm64/BubesFramework.framework/Headers/BubesFramework.h
@@ -0,0 +1,18 @@
+//
+//  BubesFramework.h
+//  BubesFramework
+//
+//  Created by Ljubomir Jankovikj on 13.7.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BubesFramework.
+FOUNDATION_EXPORT double BubesFrameworkVersionNumber;
+
+//! Project version string for BubesFramework.
+FOUNDATION_EXPORT const unsigned char BubesFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BubesFramework/PublicHeader.h>
+
+
